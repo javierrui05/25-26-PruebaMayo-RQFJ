3792c127e76cbc84e9b9e3807627e5d6e8c15ce5
javi <fruiqui413@g.educaand.es>
2026-05-07 18:35:38 +0200

    Modificacion para prueba 4

---
+++ app/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from .database import Base, engine
+from .routers import items
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+app.include_router(items.router)
+
+#Version 3
+# Código que se añade
+@app.get("/status")
+def version():
+    return {"status": "Ruiz Quirós, Francisco Javier - v.xx"}
